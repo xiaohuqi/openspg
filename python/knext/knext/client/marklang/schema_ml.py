26b18a92c448218ec5baf56110b7e888c862eb0a
matthewhyx <matthew.hyx@antgroup.com>
2024-01-05 16:49:47 +0800

    fix schema bug

--- python/knext/knext/client/marklang/schema_ml.py
+++ python/knext/knext/client/marklang/schema_ml.py
@@ -1258,14 +1258,18 @@ class SPGSchemaMarkLang:
             if spg_type_name.startswith("STD.") or spg_type_name in self.internal_type:
                 continue
 
+            sub_properties = {}
             properties = set()
             for prop, prop_type in session.get(spg_type_name).properties.items():
-                properties.add(prop)
                 if len(prop_type.sub_properties) > 0:
+                    sub_properties[prop] = set()
                     for sub_prop in prop_type.sub_properties:
-                        properties.add(f"{prop}.{sub_prop}")
+                        sub_properties[prop].add(sub_prop)
+                else:
+                    properties.add(prop)
 
             relations = set()
+            relation_sub_properties = {}
             hyp_predicate = [member.value for member in HypernymPredicateEnum]
             for relation, relation_type in session.get(spg_type_name).relations.items():
                 rel = relation.split("_")[0]
@@ -1275,16 +1279,21 @@ class SPGSchemaMarkLang:
                     or rel in session.get(spg_type_name).properties
                 ):
                     continue
-                relations.add(rel)
+
                 if len(relation_type.sub_properties) > 0:
+                    relation_sub_properties[rel] = set()
                     for sub_prop in relation_type.sub_properties:
-                        properties.add(f"{rel}.{sub_prop}")
+                        relation_sub_properties[rel].add(sub_prop)
+                else:
+                    relations.add(rel)
 
             spg_types.append(
                 {
                     "name": spg_type_name.split(".")[1],
                     "properties": properties,
+                    "sub_properties": sub_properties,
                     "relations": relations,
+                    "relation_sub_properties": relation_sub_properties,
                 }
             )
 
